8abfa8e4b03f57ad1d29e078835940fb73105a13
Joe Hallman <gohawkeyes74@yahoo.com>
2016-09-12 23:28:13 +0000

    first part of part 2

--- lib/part2.rb
+++ lib/part2.rb
@@ -4,6 +4,77 @@ class NoSuchStrategyError < StandardError ; end
 def rps_game_winner(game)
   raise WrongNumberOfPlayersError unless game.length == 2
   # YOUR CODE HERE
+  check1 = "fail"
+  check2 = "fail"
+  game[0][1] = game[0][1].upcase
+  game[1][1] = game[1][1].upcase
+  
+  if game[0][1] == "R" || game[0][1] == "P" || game[0][1] == "S"
+    check1 = "pass"
+  end
+  
+  if game[1][1] == "R" || game[1][1] == "P" || game[1][1] == "S"
+    check2 = "pass"
+  end
+  
+  raise NoSuchStrategyError unless check1 == "pass" and check2 == "pass"
+=begin  
+  if game[0][1] = "R"
+    game[0][1] = "Rock"
+  elsif game[0][1] = "P"
+    game[0][1] = "Paper"
+  elsif game[0][1] = "S"
+    game[0][1] = "Scissors"
+    
+  if game[1][1] = "R"
+    game[1][1] = "Rock"
+  elsif game[1][1] = "P"
+    game[1][1] = "Paper"
+  elsif game[1][1] = "S"
+    game[1][1] = "Scissors"
+=end
+
+  
+  if game[0][1] == "R" and game[1][1] == "P"
+    game[1][1] = "Paper"
+    return game[1]
+  end
+  if game[0][1] == "R" and game[1][1] == "S"
+    game[0][1] = "Rock"
+    return game[0]
+  end
+  if game[0][1] == "R" and game[1][1] == "R"
+    game[0][1] = "Rock"
+    return game[0]
+  end
+  if game[0][1] == "P" and game[1][1] == "R"
+    game[0][1] = "Paper"
+    return game[0]
+  end
+  if game[0][1] == "P" and game[1][1] == "S"
+    game[1][1] = "Scissors"
+    return game[1]
+  end
+  if game[0][1] == "P" and game[1][1] == "P"
+    game[0][1] = "Paper"
+    return game[0]
+  end
+  if game[0][1] == "S" and game[1][1] == "R"
+    game[1][1] = "Rock"
+    return game[1]
+  end
+  if game[0][1] == "S" and game[1][1] == "P"
+    game[0][1] = "Scissors"
+    return game[0]
+  end
+  if game[0][1] == "S" and game[1][1] == "S"
+    game[0][1] = "Scissors"
+    return game[0]
+  end
+  
+  
+  
+  
 end
 
 def rps_tournament_winner(tournament)
@@ -11,3 +82,19 @@ def rps_tournament_winner(tournament)
 end
 
 #feel free to add your own helper functions as needed
+
+tester = [["Joe", "p"], ["John", "S"]]
+
+game = rps_game_winner tester
+puts game
+=begin
+tester = ["Joe", "p"]
+
+game = rps_game_winner(tester)
+puts game
+
+tester = ["Joe", "n"]
+
+game = rps_game_winner(tester)
+puts game
+=end
