79a0d6552db530b7afc20c3eb2592aa9c1cff58c
Joe Hallman <gohawkeyes74@yahoo.com>
2016-09-13 22:01:27 +0000

    part 5

--- lib/part5.rb
+++ lib/part5.rb
@@ -2,5 +2,23 @@ class CartesianProduct
   include Enumerable
 
   # YOUR CODE HERE
+  
+  def initialize(xvar,yvar)
+    @xvar = xvar
+    @yvar = yvar
+  end
+  
+  def each
+    @xvar.each do |x|
+      @yvar.each { |y| yield [x,y] }
+      end
+  end
 
 end
+
+
+c = CartesianProduct.new([:a,:b,:c], [4,5,7,8])
+c.each { |elt| puts elt.inspect }
+
+c = CartesianProduct.new([:a,:b], [])
+c.each { |elt| puts elt.inspect }
